7c1af35008fa1cfeb080a20ba663836c8b21135c
Hiroya Kubo <hiroya@cuc.ac.jp>
2026-08-06 05:43:58 +0900

    feat(dsl4): add platform asset session (#323)
    Closes #322

--- src/dsl4/platform/index.js
+++ src/dsl4/platform/index.js
@@ -1,3 +1,4 @@
 export {createDsl4AssetManagerAdapter} from './asset-manager-adapter.js';
 export {createDsl4PlatformAssetAdapter} from './asset-adapter-router.js';
+export {createDsl4PlatformAssetSession} from './platform-asset-session.js';
 export {createDsl4TMPoseModelAdapter, createDsl4TMPosePlatform} from './tmpose-model-adapter.js';
