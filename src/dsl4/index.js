03f587b5c064aa00420b41ff96a218698acfa93d
ci <ci@example.com>
2026-08-06 02:09:55 +0900

    feat(dsl4): add keymap input adapter

--- src/dsl4/index.js
+++ src/dsl4/index.js
@@ -1,4 +1,5 @@
 export {canonicalizeDsl4Source, createDsl4SourceFrontend} from './source-frontend.js';
 export {resolveDsl4ControlProfile} from './control-profile-resolver.js';
 export {createDsl4HistoryReducer} from './history-reducer.js';
+export {createDsl4KeymapInputAdapter} from './keymap-input-adapter.js';
 export {createDsl4RuntimeController} from './runtime-controller.js';
